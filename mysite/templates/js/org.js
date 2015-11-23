7770cfb02588213f08c1f170a0654ecda59c4e7f
noticeZhou <notice_zhou@163.com>
2015-11-23 15:47:11 +0800

    update index  add register,login,org_type

--- mysite/templates/js/org.js
+++ mysite/templates/js/org.js
@@ -1,6 +1,10 @@
-var tabDiv = document.getElementsByClassName("tab")[0];
-delegateEvent(tabDiv,"a","click",tabSwich);
-delegateEvent(tabDiv,"a","mouseover",tabSwich);
+window.onload = function() {
+    var tabDiv = document.getElementsByClassName("tab")[0];
+    delegateEvent(tabDiv,"a","click",tabSwich);
+    delegateEvent(tabDiv,"a","mouseover",tabSwich);
+    login();
+}
+
 
 var oldID = "dis_1";
 function tabSwich() {
@@ -17,6 +21,18 @@ function tabSwich() {
     oldID = newID;
 }
 
+function login() {
+    var login_btn = document.getElementById("loginbox");
+    EventUtil.addHandler(login_btn,"click",function(){
+        var login_form = document.getElementById("login-form");
+        if(login_form.style.display == "none" || login_form.style.display == ""){
+            login_form.style.display = "block";
+            login_btn.style.backgroundColor = "#e6e6e6";
+        }
+        else
+            login_form.style.display = "none";
+    });
+}
 //事件代理
 function delegateEvent(element, tag, eventName, listener) {
     EventUtil.addHandler(element,eventName,function(event){
